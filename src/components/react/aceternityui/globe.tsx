1289e09092dcc6ab23af86a68848aea73aecd34d
Jack Hsu <contactjackhsu@gmail.com>
2024-06-01 21:28:17 +0100

    Update globe.tsx

--- src/components/react/aceternityui/globe.tsx
+++ src/components/react/aceternityui/globe.tsx
@@ -268,8 +268,8 @@ export function World(props: WorldProps) {
                 enableZoom={false}
                 minDistance={cameraZ}
                 maxDistance={cameraZ}
-                autoRotateSpeed={1}
-                autoRotate={true}
+                autoRotateSpeed={globeConfig.autoRotateSpeed}
+                autoRotate={globeConfig.autoRotate}
                 minPolarAngle={Math.PI / 3.5}
                 maxPolarAngle={Math.PI - Math.PI / 3}
             />
